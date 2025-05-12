ebe21a111b47c437ce0f9810054e8e3bb427cb7d
Niroj Dahal <developer.niroj@gmail.com>
2025-05-12 17:35:28 +0545

    user application access entities

---
+++ FeedbackHub.Domain/Entities/AdminUserApplicationAccess.cs
@@ -0,0 +1,17 @@
+﻿namespace FeedbackHub.Domain.Entities
+{
+    public class AdminUserApplicationAccess : BaseEntity
+    {
+        public int Id { get; set; }
+
+        public int AdminUserId { get; set; }
+        public UserDetail AdminUser { get; set; }
+
+        public int ClientId { get; set; }
+        public Client Client { get; set; }
+
+        public int? ApplicationId { get; set; } 
+        public Application Application { get; set; }
+
+    }
+}
---
+++ FeedbackHub.Domain/Entities/ClientApplicationSubscription.cs
@@ -0,0 +1,12 @@
+﻿namespace FeedbackHub.Domain.Entities
+{
+    public class ClientApplicationSubscription : BaseEntity
+    {
+        public int ClientId { get; set; }
+        public int ApplicationId { get; set; }
+
+        public Client Client { get; set; }
+
+        public Application Application { get; set; }
+    }
+}

e6980ec621c74e18a6053cbee2e3a550ea59db1d
Niroj Dahal <developer.niroj@gmail.com>
2025-05-18 00:30:16 +0545

    logged in user shown in admin navbar

--- feedbackhub.client/src/pages/Admin/AdminLayout.tsx
+++ feedbackhub.client/src/pages/Admin/AdminLayout.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Sidebar from './Sidebar';
+import AdminNavbar from './Navbar';
 
 const AdminLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div className="container-fluid" style={{ position: 'absolute', top: 0, left: 0, right: 0 }}>
-      <Navbar />
-      <div className="row flex-nowrap" style={{ marginTop: '3.5rem' }}>
+      <AdminNavbar />
+      <div className="row flex-nowrap" style={{ marginTop: '4rem' }}>
         <Sidebar />
         <div className="col" style={{ marginTop: '2rem' }}>
           {children}
--- feedbackhub.client/src/pages/Admin/Navbar.tsx
+++ feedbackhub.client/src/pages/Admin/Navbar.tsx
@@ -1,32 +1,62 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { handleLogout } from '../../services/AuthService';
+import { Navbar, Nav, Container, NavDropdown, Button } from 'react-bootstrap';
+import { useUser } from '../../contexts/UserContext';
 
-const Navbar = () => {
+const AdminNavbar = () => {
   const navigate = useNavigate();
+    const { user } = useUser();
 
   const logoutButtonClicked = () => {
    handleLogout(navigate);
   };
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-    <div className="container-fluid">
-      <a className="navbar-brand" href="#">Feedback Hub</a>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav ms-auto">          
-          <li className="nav-item">
-            <a className="nav-link" href="#" onClick={logoutButtonClicked}>Logout</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+     <Navbar variant="dark"  bg="dark" expand="md" fixed="top" className='admin-navbar'>
+      <Container fluid>
+        <Button variant="primary" className="d-lg-none">
+          ☰
+        </Button>
 
+        <Navbar.Brand className="ms-2" href="#">
+          Feedback Hub
+        </Navbar.Brand>
+
+        {/* <Navbar.Toggle aria-controls="navbarScroll" /> */}
+        <Navbar.Collapse id="navbarScroll">
+          <Nav className="ms-auto align-items-center">
+            {/* Profile Dropdown */}
+            <NavDropdown
+              title={
+                <span className="d-flex align-items-center">
+                  <img
+                    src= '/assets/images/default-user.png' 
+                    alt="Profile"
+                    className="rounded-circle me-2"
+                    style={{ width: '30px', height: '30px' }}
+                  />
+                  {user?.Fullname}
+                </span>
+              }
+              id="profileDropdown"
+              align="end"
+            >
+
+              <NavDropdown.Item href="#">Profile</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="/profile/change-password" className='text-warning'>Change Password</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#" className="text-danger" onClick={logoutButtonClicked}>
+                Logout
+              </NavDropdown.Item>
+            </NavDropdown>
+
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
-export default Navbar;
+export default AdminNavbar;
